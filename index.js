10bf9c0139d5e74f85b15853cb35bb7c71ceaa98
hiimbex <hiimbex@github.com>
2017-06-14 11:50:12 -0700

    got bot commenting on issues

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 module.exports = robot => {
-  robot.on('issues.opened', async (event, context) => {
-    // Code was pushed to the repo, what should we do with it?
-    robot.log(event["payload"["comment"]]);
+  robot.on('issues', async (event, context) => {
+    //robot.log(typeof event);
+    //const template = "Congrats on opening a PR ";
+    //const fin_temp = JSON.stringify(event);
+    let issue = event.payload.issue || event.payload.pull_request;
+    if (!issue.number) {
+       issue = (await context.github.issues.get(context.issue())).data;
+     }
+    
+    //this won't really work ;(
+    //Gotta figure out milestones in the GitHub API
+    if (event.payload.issue.number == 1) {
+        return context.github.issues.createComment(context.issue({body: "Congrats on your first PR/Issue!"}));
+    } else {
+        return context.github.issues.createComment(context.issue({body: "Thanks for opening an Issue!"}));
+    }
+    // template = "conrgats on your first PR/Issue!"
+
+    //robot.log(test_temp);
   });
 };
