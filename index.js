9aa8e398bf723c1dd41f84f6bb00843a0ce29aec
hiimbex <hiimbex@github.com>
2017-06-16 14:52:06 -0700

    successfully congratulates new users on their first pr/issue

--- index.js
+++ index.js
@@ -1,37 +1,29 @@
 //probot run -P test-bot.2017-06-13.private-key.pem  ./index.js
 module.exports = robot => {
-  robot.on('issues', async (event, context) => {
-    //should be issues.opened to guarantee new issues
-    //but included editted issues for purposes of debigging rn
-    robot.log("This is a debug message");
-    robot.log.debug("So is this!");
-    //robot.log(event);
-    let issue = event.payload.issue || event.payload.pull_request;
-    if (!event.payload.repository.owner || !issue.user.id || !event.payload.repository.name) {
+  robot.on('issues.opened', async context => {
+
+    robot.log("This is a debug test message");
+
+    let issue = context.payload.issue || context.payload.pull_request;
+    if (!context.payload.repository.owner || !issue.user.id || !context.payload.repository.name) {
         issue = (await context.github.issues.get(context.issue())).data;
     }
-    const user_login = event.payload.issue.user.login;
-    const repo_owner_id = event.payload.repository.owner.login;
-    const repo_name = event.payload.repository.name;
-    robot.log(event.payload.issue.user.id);
-
-    //Do GitHub API check based on this user's ID
-    // GET /repos/:owner/:repo/issues
-    // Parameters: creator = login or user id?
-    // state = all
-
+    const user_login = context.payload.issue.user.login;
+    const repo_owner_id = context.payload.repository.owner.login;
+    const repo_name = context.payload.repository.name;
+    
     var GitHubApi = require("github");
-
+    
     var github = new GitHubApi({
         debug: true,
     });
-
+    
     github.authenticate({
         type: "oauth",
         key: process.env.CLIENT_ID,
         secret: process.env.CLIENT_SECRET
     });
-
+    
     github.issues.getForRepo({
         owner: repo_owner_id,
         repo: repo_name,
@@ -41,18 +33,12 @@ module.exports = robot => {
         if (error) {
             console.log(error.toJSON());
         } else {
-            robot.log(response.data);
+            //robot.log(response.data.length);
             //check length of response to make sure its only one issue/pr
+            if (response.data.length === 1) {
+                context.github.issues.createComment(context.issue({body: "Congrats on your first PR/Issue!"}));
+            }
         }
     });
-
-    if (event.payload.issue.number == 1) {
-        return context.github.issues.createComment(context.issue({body: "Congrats on your first PR/Issue!"}));
-    } else {
-        return context.github.issues.createComment(context.issue({body: "Thanks for opening an Issue!"}));
-    }
-    // template = "conrgats on your first PR/Issue!"
-
-    //robot.log(test_temp);
   });
 };
