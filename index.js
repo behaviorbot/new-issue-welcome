80686c849bbc5b94f85b7e1505b9b55f12ff63fb
wldn <wildansnahar@gmail.com>
2017-09-29 15:55:02 +0700

    swap to standard linter

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 module.exports = robot => {
-    robot.on('issues.opened', async context => {
-        const response = await context.github.issues.getForRepo(context.repo({
-            state: 'all',
-            creator: context.payload.issue.user.login
-        }));
+  robot.on('issues.opened', async context => {
+    const response = await context.github.issues.getForRepo(context.repo({
+      state: 'all',
+      creator: context.payload.issue.user.login
+    }))
 
-        const countIssue = response.data.filter(data => !data.pull_request);
-        if (countIssue.length === 1) {
-            try {
-                const config = await context.config('config.yml');
-                if (config.newIssueWelcomeComment) {
-                    context.github.issues.createComment(context.issue({body: config.newIssueWelcomeComment}));
-                }
-            } catch (err) {
-                if (err.code !== 404) {
-                    throw err;
-                }
-            }
+    const countIssue = response.data.filter(data => !data.pull_request)
+    if (countIssue.length === 1) {
+      try {
+        const config = await context.config('config.yml')
+        if (config.newIssueWelcomeComment) {
+          context.github.issues.createComment(context.issue({body: config.newIssueWelcomeComment}))
         }
-    });
-};
+      } catch (err) {
+        if (err.code !== 404) {
+          throw err
+        }
+      }
+    }
+  })
+}
